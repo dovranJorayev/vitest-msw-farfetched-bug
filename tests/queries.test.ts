0533d57ed38f887c2f903259c519c0911c19a91a
dovran <sony.energizer@gmail.com>
2025-04-07 19:03:04 +0500

    Update API URL in queries test cases to use localhost and add new test for fetch with Request.

--- tests/queries.test.ts
+++ tests/queries.test.ts
@@ -15,7 +15,7 @@ import {
     msw: [
       async ({}, use) => {
         const handlers = [
-          http.get('/api/v1/profile', () => {
+          http.get('http://localhost:3000/api/v1/profile', () => {
             return HttpResponse.json({
               name: 'test user',
             });
@@ -45,7 +45,7 @@ import {
   
       const jsonQuery = createJsonQuery({
         request: {
-          url: '/api/v1/profile',
+          url: 'http://localhost:3000/api/v1/profile',
           method: 'GET',
         },
         response: {
@@ -64,7 +64,7 @@ import {
       const scope = fork();
   
       const jsonQuery = createQuery({
-        handler: () => fetch('/api/v1/profile').then((res) => res.json()),
+        handler: () => fetch(new Request('http://localhost:3000/api/v1/profile')).then((res) => res.json()),
       });
   
       await allSettled(jsonQuery.start, {
@@ -73,5 +73,16 @@ import {
       expect(scope.getState(jsonQuery.$error)).toBeNull();
       expect(scope.getState(jsonQuery.$data)).toEqual({ name: 'test user' });
     });
+    itMocked('fetch with Request', async () => {
+      const responseWithUrl = await fetch(
+        'http://localhost:3000/api/v1/profile'
+      ).then((res) => res.json());
+      
+      const responseWithRequest  = await fetch(
+        new Request('http://localhost:3000/api/v1/profile', )
+      ).then((res) => res.json());
+
+      expect(responseWithUrl).toEqual({ name: 'test user' });
+      expect(responseWithRequest).toEqual({ name: 'test user' });
+    })
   });
-  
